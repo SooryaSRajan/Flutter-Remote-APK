5958613d7478e5938687d23c91bd5d0163ede553
sooryasrajan <soorya.s27@gmail.com>
2022-06-27 11:36:27 +0530

    Completed index.js code to clone a git repository and build apk

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const URL = require("url").URL;
 function OSFunc() {
     this.execCommand = function (cmd, callback) {
         exec(cmd, (error, stdout, stderr) => {
+            console.log(stdout, stderr)
             if (error) {
+                console.error(`exec error: ${error}`);
                 callback(true, stderr);
                 return;
             }
@@ -39,6 +41,7 @@ app.get("/", (req, res) => {
 })
 
 app.get("/cloneGitHub", (req, res) => {
+
     console.log(req.query.url)
 
     const url = req.query.url
@@ -49,17 +52,27 @@ app.get("/cloneGitHub", (req, res) => {
     if(!stringIsAValidUrl(url)){
         return res.status(400).send("Invalid URl")
     }
-
     os.execCommand("rm -rf FlutterCode", function (err, response) {
-        if(err) return res.status(200).send(response)
+        if(err) return res.status(400).send(response)
         os.execCommand(`git clone ${url} FlutterCode`, function (err, response){
             if(err){
-                return res.send("Could not clone into " + url + " error: " + response)
+                return res.status(400).send("Could not clone into " + url + " error: " + response)
             }
-            //TODO: Run build command here
-            res.send(response)
+            os.execCommand(`cd FlutterCode && ls && flutter build apk -v`, function (err, response){
+                console.log(response)
+                if(err){
+                    return res.status(400).send("Could not build flutter APK, " + " error: " + response)
+                }
+                else{
+                    return res.send("APK build successfully!")
+                }
+            })
         })
     })
+})
+
+app.get("/apk", (req, res) => {
+    res.download(__dirname + "/FlutterCode/build/app/outputs/flutter-apk/app.apk", "app.apk")
 })
 
 app.listen(8080, () => {
